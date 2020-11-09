93b7e14a5c9d775348e360c4841cbc0e2346d874
Markus Minton <markus.minton@usaa.com>
2020-11-09 17:59:37 +0000

    added orders model

--- models/customers.sql
+++ models/customers.sql
@@ -10,21 +10,27 @@ orders as (
 
 ),
 
-customer_orders as (
+payments as (
 
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
+    select * from {{ ref('stg_payments') }}
 
 ),
 
+customer_orders as (
+
+    select
+        ord.customer_id
+        ,min(ord.order_date) as first_order_date
+        ,max(ord.order_date) as most_recent_order_date
+        ,count(ord.order_id) as number_of_orders
+        ,sum(pmt.amount) as payment_amount
+    from orders ord
+    left join payments pmt
+        on ord.order_id = pmt.order_id
+        and pmt.status = 'success'
+    group by 1
+
+),
 
 final as (
 
@@ -34,10 +40,9 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.payment_amount,0) as lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
 
 )
